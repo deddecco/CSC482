1d41358f87a717e3a802641004ed2809f684baf3
Andre da Silva <apdasil@emory.edu>
2025-01-13 12:55:54 -0500

    make method recursive

--- src/main/java/DominoTromino.java
+++ src/main/java/DominoTromino.java
@@ -1,53 +1,44 @@
 import java.util.HashMap;
 
-
-// solves LeetCode 790
-// 39 / 39 testcases passed (on LeetCode)
 public class DominoTromino {
      private static final int MOD = 1000000007;
+     private final HashMap<Integer, Integer> memo = new HashMap<>();
 
      public static void main(String[] args) {
           DominoTromino dt = new DominoTromino();
-
           int numTilings = dt.numTilings(3);
           System.out.println(numTilings);
      }
 
      public int numTilings(int n) {
-          // base cases-- one way to do nothing, 2 ways to do 2x2, 5 ways to do 3x2
+          // Base cases
+          // there is 1 way to do nothing
           if (n == 0) {
-               // do nothing
                return 1;
           }
+          // 2by1 has to go in the direction of the grid
+          // 2by2 can go either 2 flat or 2 upright
           if (n == 1 || n == 2) {
-               // 2x1 is only OK in the orientation of the grid
-               // 2x2 lets the dominos be upright or flat
                return n;
           }
+          // upright, 2 stacked flats
+          // 2 stacked flats, upright
+          // 3 upright
+          // "PJ" with a tromino
+          // "L7" wth a tromino
           if (n == 3) {
-               // trominos that look like "PJ" or "L7" = 2
-               // upright, 2 stacked flat = 1
-               // 2 stacked flat, upright = 1
-               // 3 upright = 1
-               // 2 + 1 + 1 + 1 = 5
                return 5;
           }
 
-
-          // construct the map from the base cases
-          HashMap<Integer, Integer> map = new HashMap<>();
-          map.put(0, 1);
-          map.put(1, 1);
-          map.put(2, 2);
-          map.put(3, 5);
-
-          // for 2x4 or greater, the number is 2x how many for n-1 + how many for n-3
-          for (int i = 4; i <= n; i++) {
-               int value = (int) (((2L * map.get(i - 1)) + map.get(i - 3)) % MOD);
-               // once a value is computed, put it into the map
-               map.put(i, value);
+          // efficient retrieval-- cut down on duplicate work
+          if (memo.containsKey(n)) {
+               return memo.get(n);
           }
 
-          return map.get(n);
+          // recursive call
+          long result = ((long) 2 * numTilings(n - 1) + numTilings(n - 3)) % MOD;
+          int value = (int) result;
+          memo.put(n, value);
+          return value;
      }
 }
